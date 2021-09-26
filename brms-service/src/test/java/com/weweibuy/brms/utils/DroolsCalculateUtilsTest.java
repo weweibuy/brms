643c915ffe560ae73082de5a29af21e378a74e01
杜仁浩 <ak514250@126.com>
2021-09-26 23:34:11 +0800

    Aviator  test

--- brms-service/src/test/java/com/weweibuy/brms/utils/DroolsCalculateUtilsTest.java
+++ brms-service/src/test/java/com/weweibuy/brms/utils/DroolsCalculateUtilsTest.java
@@ -14,8 +14,12 @@ public class DroolsCalculateUtilsTest {
         Map env = new HashMap<>();
         env.put("price", new BigDecimal(1000));
         env.put("qty", new BigDecimal(3));
+        env.put("qty2", new BigDecimal("3.1"));
         Object execute = AviatorEvaluator.execute("price / qty + qty", env);
+        Object executeB = AviatorEvaluator.execute("1 > 0");
+        Object execute1 = AviatorEvaluator.execute("qty < qty2 && qty > qty2 + 100", env);
         System.out.println(execute);
+        System.out.println(execute1);
     }
 
 }
